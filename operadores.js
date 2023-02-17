0659c9c69572fa12f2570d3629258acc5603a179
exequielsosa <sosae1759@gmail.com>
2023-02-17 00:10:06 -0300

    Esto es parte del aprendizaje

---
+++ operadores.js
@@ -0,0 +1,12 @@
+console.log("Hola" + "Exequiel");
+var nombre = 'Exequiel';
+var saludo = 'Hola' + nombre;
+console.log(saludo);
+
+console.log('73' < '9')
+
+
+console.log("Hello" + "Facha")
+var saludo = "Hello" + nombre
+var nombre = "Facha"
+console.log(saludo)
